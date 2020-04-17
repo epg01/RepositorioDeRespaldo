cd7e8aca9f241915a4fca1ccbc9ae8ad1f29dab7
epg01 <emmanuelpalacio1@hotmail.com>
2020-04-17 18:28:49 +0000

    Exercise solved, here is the getline function is a primitive phase

---
+++ EmmanuelPalacioGaviria/Chapter1/Section1.9/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define MAXLINE 1000     /* Maximun input line length*/
+
+int getlinee(char line[], int maxline);
+void copy(char to[], char from[]);
+
+// Print the longest input line
+int main(void)
+{
+	int len;                      // Current line length
+	int max;                      // Maximun length see so far
+	char line[MAXLINE];           // Current input line
+	char longest[MAXLINE];        // longest line saved here
+
+	max = 0;
+	while ((len = getlinee(line, MAXLINE)) > 0)
+		if (len > max){
+			max = len;
+			copy(longest, line);
+		}
+	if (max > 0)                  // There was a line
+		printf("%s", longest);
+	return(0);
+}
