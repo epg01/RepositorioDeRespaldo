f6d22725bbdd04a4aec40db601ff0282bf2c92bc
Emmanuel Palacio Gaviria <emmanuelpalacio1@gmail.com>
2020-05-03 14:30:49 +0000

    Un codigo mas limpio, falta resolver un error de salida

--- Chapter1/Ejercicios/Exercise1-16.c
+++ Chapter1/Ejercicios/Exercise1-16.c
@@ -11,69 +11,95 @@
  *     ***********************************************************************************************************
  */
 
+/* Inlcuimos la librería stdio.h -> Aun no entiendo bien las funcionalidades de esta librería 
+   pero sé que esta librería define la entrada y salida de funciones, tipos y macros. (Tengo que profundizar más en esto.)*/
+
 #include <stdio.h>
 
-#define MAXLINE 5                           /* maximum input line length */
+/* Sé define una contante simbolica, ya que es una mala practica enterrar número mágicos*/
 
-int getlinee(char line[], int maxline);
-void copy(char to[], char from[]);
+#define NUMBERMAXIMUMCHARACTERES 3
 
-/* print the longest input line */
+/* Sé declaran dos funciones, ya que especifica el tipo de argumentos que le llegan y el tipo de dato que retornará la función 
+   nota: aun no hemos definido la funcion, como se declaran las funciones es optativo en los parámetros asignar un nombre de variable al tipo de dato, pero esto
+          es una mala practica ya que da poca documentación o informacion al programa. */
 
-main()
+int getlinee(char StoredCharacter[], int SizeArray);    /* En donde como hemos dicho pudimos hacer escrito int getline(char [], int) ya que no estamos
+							  definiendo a la función si no declarandola, inclusive tambien se pude hacer escrito
+						          getline(char [], int) si no se coloca el tipo de dato el compilador asumirá que será de tipo int*/
+void copy(char LargestLineSeeSoFar[], char StoresLineLongest[]);
+// Nota: Las funciones que se declaran, comunmente arriva de la funcion principal (main), se denomina comunmnete función prototipo (function prototype)
+
+int main (void) // Sé definio una función y estoy seguro que también se declara.
 {
-	int len;                            /* current line length */
-	char line[MAXLINE];                 /* current input line */
-	char longest[MAXLINE];              /* longest line saved here */
-	int AccountantNumberBiggerMaxline;
-	int StoredNumberBiggerMaxline;
-	int NumberLessMaxline;
+	int SizeLineCurrent;
+	char StoredCharacter[NUMBERMAXIMUMCHARACTERES];
+	char LargestLineSoSeeFar[NUMBERMAXIMUMCHARACTERES];
+	int MaximumLineSeeSoFar;
+	int AccountantOverflowNUMBERMAXIMUMCHARACTERES;
+	int StoredAccountant;
 
-	NumberLessMaxline = AccountantNumberBiggerMaxline = StoredNumberBiggerMaxline = 0;
-	while ((len = getlinee(line, MAXLINE)) > 0)
+	 AccountantOverflowNUMBERMAXIMUMCHARACTERES = MaximumLineSeeSoFar = 0;
+	while ((SizeLineCurrent = getlinee(StoredCharacter, NUMBERMAXIMUMCHARACTERES)))
 	{
-		if (line[len - 1] != '\n')
-			AccountantNumberBiggerMaxline += len;
-		else if (AccountantNumberBiggerMaxline)
-			AccountantNumberBiggerMaxline += len;
 
-		if (len > NumberLessMaxline) {
-			NumberLessMaxline = len;
-			copy(longest, line);
+/* Esta condición es importante, ya que verifica si hay un desbordamiento, es decir si aún hay caracteres que hacen parte de una línea y no han sido almacenamos en
+   su totalidad por el vector, ya que tiene un tamaña fijo (finito). Lo importante de esta condición if-else-if, es que incluimos en el conteo el final de línea,
+   ya que por definición es un carácter visible (es decir se cuenta) ya que el carácter terminador \0 no se incluye en el conteo.*/
+
+		if (SizeLineCurrent == (NUMBERMAXIMUMCHARACTERES - 1) && !(StoredCharacter[SizeLineCurrent - 1] == '\n'))
+			AccountantOverflowNUMBERMAXIMUMCHARACTERES += SizeLineCurrent;
+		else if (StoredCharacter[SizeLineCurrent - 1] == '\n')
+			AccountantOverflowNUMBERMAXIMUMCHARACTERES += SizeLineCurrent;
+
+		if ((SizeLineCurrent > MaximumLineSeeSoFar) && (AccountantOverflowNUMBERMAXIMUMCHARACTERES > StoredAccountant)){
+			MaximumLineSeeSoFar = SizeLineCurrent;
+			copy(LargestLineSoSeeFar, StoredCharacter);
 		}
-/* Algo muy sutil es que esta condicion tiene que despues de la condicion if (len > NumberLessMaxline) */
-/* Cuando getchar encuentra el salto de linea, inicializamos la variable AccountatNumber... para su proximo conteo y guardamos el numero mas grande de AccountantNumber...
-   Tambien inicializamos la variables NumberLessMaxline ¿saben por que?  */
 
-                if ((AccountantNumberBiggerMaxline > StoredNumberBiggerMaxline) && (line[len - 1] == '\n'))
-                        StoredNumberBiggerMaxline = AccountantNumberBiggerMaxline, AccountantNumberBiggerMaxline = NumberLessMaxline = 0;
+/* La condición almacena los caracteres contados hasta ahora (la máxima cantidad). */
+
+		if ((AccountantOverflowNUMBERMAXIMUMCHARACTERES > StoredAccountant))
+			StoredAccountant = AccountantOverflowNUMBERMAXIMUMCHARACTERES;
+
+/* Cuando se llega al salto de línea, carácter nuevo línea, inicializamos las variables a sus valores iniciales para su nuevo y seguro conteo. */
+		if (AccountantOverflowNUMBERMAXIMUMCHARACTERES && (!(StoredCharacter[SizeLineCurrent - 1] != '\n')))
+			AccountantOverflowNUMBERMAXIMUMCHARACTERES = MaximumLineSeeSoFar = 0;
 	}
-	if (StoredNumberBiggerMaxline > 0)
-		printf("%s\nLongitud de la linea mas larga copiada: %d\n", longest, StoredNumberBiggerMaxline);
-	else if (NumberLessMaxline > 0)
-		printf("%s", longest);
+
+/* La condición if-else verifica si hubo un desbordamiento he imprime la línea más larga que el array pudo almacenar. */
+
+	if (StoredAccountant > 0)
+		printf("%s\nLongitud de la linea mas larga copiada: %d\n", LargestLineSoSeeFar, StoredAccountant);
+	else
+		printf("%s", LargestLineSoSeeFar);
 	return 0;
 }
 
-/* getline: read a line into s, return length */
-int getlinee(char s[],int lim)
+// Obtiene nueva línea
+
+getlinee(char StoredCharacter[], int SizeArray)
 {
-	int c, i;
-	for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
-		s[i] = c;
-	if (c == '\n') {
-		s[i] = c;
-		++i;
-	}
-	s[i] = '\0';
-	return i;
+	int i, InputCharacter;
+
+	for (i = 0; i < (SizeArray - 1) && ((InputCharacter = getchar()) != EOF) && (InputCharacter != '\n'); i++)
+		StoredCharacter[i] = InputCharacter;
+
+	if (InputCharacter == '\n')
+		StoredCharacter[i++] = InputCharacter;
+
+	StoredCharacter[i] = '\0';
+
+	return(i);
 }
 
-/* copy: copy 'from' into 'to'; assume to is big enough */
-void copy(char to[], char from[])
+// Utilizamos el concepto de artimética de punteros -> esta aritmética contiene la parte de aritmetica de array, en por definición es una parte de la artimética de
+// Punteros.
+
+// copia la línea mas larga vista has ahoara.
+
+void copy(char LargestLineSeeSoFar[], char StoresLineLongest[])
 {
-	int i;
-	i = 0;
-	while ((to[i] = from[i]) != '\0')
-		++i;
+	while((*LargestLineSeeSoFar++ = *StoresLineLongest++))
+		;
 }
